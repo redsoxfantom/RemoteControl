d5ac5e61746542f4809d617f28a25da3dd9d20de
Tom <redsoxfantom@gmail.com>
2016-01-25 22:10:50 -0500

    Created skeleton interfaces for worker classes

---
+++ RemoteControlServer/RemoteControlServer/Listeners/IListener.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteControlServer.Listeners
+{
+    /// <summary>
+    /// Interface for all classes responsible for receiving data from a client device.
+    /// Data received is passed off to a Translator
+    /// </summary>
+    public interface IListener
+    {
+
+    }
+}
---
+++ RemoteControlServer/RemoteControlServer/Translators/ITranslator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteControlServer.Translators
+{
+    /// <summary>
+    /// Interface for all classes responsible for turning data received from a listener to commands for an implementer
+    /// </summary>
+    public interface ITranslator
+    {
+    }
+}
