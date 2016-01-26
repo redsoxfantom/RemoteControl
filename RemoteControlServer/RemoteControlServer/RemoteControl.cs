2d94dd80f4ef16519536338aa52a6d175f055fe3
Tom <redsoxfantom@gmail.com>
2016-01-25 22:11:09 -0500

    Created main class

---
+++ RemoteControlServer/RemoteControlServer/RemoteControl.cs
@@ -0,0 +1,20 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteControlServer
+{
+    class RemoteControl
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(RemoteControl));
+
+        static void Main(string[] args)
+        {
+            XmlConfigurator.Configure();
+        }
+    }
+}
