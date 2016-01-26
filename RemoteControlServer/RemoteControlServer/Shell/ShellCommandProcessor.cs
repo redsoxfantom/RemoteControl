8ec4d40acf93b0a2c87171f59ec9b7e0b48a4f45
Tom <redsoxfantom@gmail.com>
2016-01-25 22:32:53 -0500

    Added dictionary mapping commands to their input strings

--- RemoteControlServer/RemoteControlServer/Shell/ShellCommandProcessor.cs
+++ RemoteControlServer/RemoteControlServer/Shell/ShellCommandProcessor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RemoteControlServer.Shell.Commands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,11 @@ namespace RemoteControlServer.Shell
     public class ShellCommandProcessor
     {
         private static ShellCommandProcessor mInstance = null;
+
+        /// <summary>
+        /// Maps an input string to it's expected command
+        /// </summary>
+        private Dictionary<String, ICommand> mLoadedCommands;
 
         public ShellCommandProcessor Instance
         {
