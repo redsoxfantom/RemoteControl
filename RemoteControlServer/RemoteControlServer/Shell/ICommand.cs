7bb1eb06743c369450b4086ff39c9db802a97a08
Tom <redsoxfantom@gmail.com>
2016-01-25 22:20:51 -0500

    Created interface for any commands entered into the shell

---
+++ RemoteControlServer/RemoteControlServer/Shell/ICommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteControlServer.Shell
+{
+    /// <summary>
+    /// Interface for all shell commands
+    /// </summary>
+    interface ICommand
+    {
+        void Execute();
+    }
+}
