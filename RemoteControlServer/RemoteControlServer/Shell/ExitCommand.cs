8cd70d63ab43adb2f6ab760863702f962a428e92
Tom <redsoxfantom@gmail.com>
2016-01-25 22:24:38 -0500

    Created singleton to process commands from the shell

---
+++ RemoteControlServer/RemoteControlServer/Shell/ExitCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteControlServer.Shell
+{
+    /// <summary>
+    /// Class executed when user types "done"
+    /// </summary>
+    public class ExitCommand : ICommand
+    {
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RemoteControlServer/RemoteControlServer/Shell/ShellCommandProcessor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteControlServer.Shell
+{
+    /// <summary>
+    /// Singleton class that executes commands from the Shell
+    /// </summary>
+    public class ShellCommandProcessor
+    {
+        private static ShellCommandProcessor mInstance = null;
+
+        public ShellCommandProcessor Instance
+        {
+            get
+            {
+                if(mInstance == null)
+                {
+                    mInstance = new ShellCommandProcessor();
+                }
+                return mInstance;
+            }
+        }
+        private ShellCommandProcessor()
+        {
+
+        }
+    }
+}
