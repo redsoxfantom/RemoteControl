d95f1af8defd98b0e21749972b38846bd57d52a2
Tom <redsoxfantom@gmail.com>
2016-03-09 19:50:11 -0500

    Work on transmitter

---
+++ RemoteControlServer/RemoteControlServer/Networking/BeaconTransmitter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteControlServer.Networking
+{
+    public class BeaconTransmitter
+    {
+        UdpClient client;
+
+        public BeaconTransmitter()
+        {
+            client = new UdpClient();
+        }
+    }
+}
