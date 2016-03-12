7f2536eff3e4693fb0fd5acc144f3f60a333f1b4
Tom <redsoxfantom@gmail.com>
2016-03-11 20:09:14 -0500

    Work on creating a help command

---
+++ RemoteControlServer/RemoteControlServer/Shell/Commands/HelpCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteControlServer.Shell.Commands
+{
+    [ShellInput("Help")]
+    [ShellInput("help")]
+    [ShellInput("?")]
+    public class HelpCommand : ICommand
+    {
+        public void Execute(Processor processor, params string[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ShowLongHelp()
+        {
+            return "Displays command information.\n" +
+                   "Parameters: [command name]\n" +
+                   "    [command name] - Get information about a specific command\n" +
+                   "        If no command is given, returns all commands known to the system";
+        }
+
+        public string ShowShortHelp()
+        {
+            return "Displays command information";
+        }
+    }
+}
